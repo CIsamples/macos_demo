eab8336868f7c2613a3d42645d1ba00d43d5559f
nofarb <nofarb@gmail.com>
2023-03-23 11:30:56 -0500

    added project files'

---
+++ macos_hello_world/macos_hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  macos_hello_worldApp.swift
+//  macos_hello_world
+//
+//  Created by Nofar Bluestein on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct macos_hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macos_hello_worldUITests/macos_hello_worldUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  macos_hello_worldUITestsLaunchTests.swift
+//  macos_hello_worldUITests
+//
+//  Created by Nofar Bluestein on 3/23/23.
+//
+
+import XCTest
+
+final class macos_hello_worldUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
